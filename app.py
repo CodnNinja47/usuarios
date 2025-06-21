ac511a7bc53548552f80e81c7a587f1096a63a7e
CodnNinja47 <154836594+CodnNinja47@users.noreply.github.com>
2025-06-20 21:14:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # 👈 Importa CORS
+from flask_cors import CORS
 import tu_script
+import os
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app)
 
 @app.route('/')
 def index():
@@ -15,3 +16,7 @@ def buscar_usuario():
     usuario = data.get("usuario")
     resultado = tu_script.buscar(usuario)
     return jsonify(resultado)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
